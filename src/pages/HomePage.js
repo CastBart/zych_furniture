64918512c04515f2b98345a26ec236c2d5728d9f
CastBart <bartosz.j.zych@gmail.com>
2022-05-06 13:30:22 +0100

    Retrieving information from database for landing page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,6 @@
-import { useState } from "react";
 import MyCarousel from "../components/MyCarousel/MyCarousel";
 
 const HomePage = () => {
-  const [landingImageURL, setLandingImageURL] = useState("");
 
   return <MyCarousel />;
 };
